b2d5b4f3414cd81f5e2bdba09e58f65e8ceb329e
gicontz <gimelcontz@gmail.com>
2021-09-06 23:30:15 +0800

    User change

--- SecretStorage.py
+++ SecretStorage.py
@@ -8,7 +8,7 @@ ScholarsDict = {
     
     # The following are placeholders...
     os.environ.get('ALPHA_ISKO') : ['Josh | Axie Pixel - Alpha', '0x5cf87ecafb97ef82ff26563879ef364d11a5bb6c', os.environ.get('ALPHA_SK')],
-    os.environ.get('BETA_ISKO') : ['ghilo17 | Axie Pixel - Alpha', '0x3e108a8776613656ffafce31da3dc88c3538f016', os.environ.get('BETA_SK')],
+    '698092015237201990' : ['ghilo17 | Axie Pixel - Alpha', '0x3e108a8776613656ffafce31da3dc88c3538f016', os.environ.get('BETA_SK')],
     os.environ.get('GAMMA_ISKO') : ['Kyle | Axie Pixel - Gamma', '0x23a7149ff395c0649ce739b8dc62b98a78e09c05', os.environ.get('GAMMA_SK')],
     os.environ.get('DELTA_ISKO') : ['Grace | Axie Pixel - Delta', '0xfa54eda8f2213c579964983d3fdbbe6bb7ee2fd3', os.environ.get('DELTA_SK')],
     os.environ.get('EPSILON_ISKO') : ['Luis | Axie Pixel Gamma', '0xb34fd9e5b6f531282091519b90a038b01d9c9f98', os.environ.get('EPSILON_SK')],
